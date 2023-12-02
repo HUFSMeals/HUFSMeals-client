c741b2eb09ef3b91a836e6f66af62223c31b88bf
zizonyoungjun <naya700@naver.com>
2023-12-02 22:11:52 +0900

    Initial Setting

--- HUFSMEALS-Client/src/App.tsx
+++ HUFSMEALS-Client/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import GlobalStyle from './styles/globalStyles';
+import { ThemeProvider } from 'styled-components';
+import { RouterProvider } from 'react-router-dom';
+import { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
+import Layout from './components/Layout';
+import { theme } from './styles/theme';
+import Router from './components/Router';
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ThemeProvider theme={theme}>
+        <RecoilRoot>
+          <GlobalStyle />
+          <Layout>
+            <Suspense>
+              <RouterProvider router={Router} />
+            </Suspense>
+          </Layout>
+        </RecoilRoot>
+      </ThemeProvider>
     </>
-  )
+  );
 }
-
-export default App
